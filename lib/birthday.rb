01bdeb999c1efa79afe974654692d6f79dbffda1
Troy Gooden <tro12185@gmail.com>
2019-03-23 22:14:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kid}! you are now #{age} years old!"
     else
-      puts "You are too old for this."
+      puts "you are too old for this."
     end 
   end 
 end 
